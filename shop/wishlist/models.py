4f317e14c8d64c98e4655f341e6ad15d928e0335
Антон <asenchuk.lex@yandex.ru>
2023-09-11 23:14:30 +0300

    Новое приложение wishlist, реализовано добавление, удаление товара из избранного

---
+++ shop/wishlist/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from profiles.models import User
+from products.models import Product
+
+
+class Wishlist(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class WishlistItem(models.Model):
+    wishlist = models.ForeignKey(Wishlist, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+    date_add = models.DateTimeField(auto_now_add=True)
---
+++ shop/wishlist/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, HttpResponseRedirect, reverse
+from django.contrib.auth.decorators import login_required
+from wishlist.models import Wishlist, WishlistItem
+from products.models import Product
+
+
+@login_required
+def add_to_wishlist(request, product_id):
+    user = request.user
+    product = Product.objects.get(id=product_id)
+    wishlist, created = Wishlist.objects.get_or_create(user=user)
+
+    # Проверка наличия товара в корзине
+    wishlist_item, item_created = WishlistItem.objects.get_or_create(wishlist=wishlist, product=product)
+    if not item_created:
+        wishlist_item.quantity += 1
+        wishlist_item.save()
+
+    return HttpResponseRedirect(reverse('wishlist:wishlist'))
+
+
+@login_required
+def del_from_wishlist(request, product_id):
+    user = request.user
+    product = Product.objects.get(id=product_id)
+    wishlist = Wishlist.objects.get(user=user)
+    wishlist_item = WishlistItem.objects.get(wishlist=wishlist, product=product)
+    wishlist_item.delete()
+    return HttpResponseRedirect(reverse('wishlist:wishlist'))
+
+
+@login_required
+def wishlist_view(request):
+    wishlist, created = Wishlist.objects.get_or_create(user=request.user)
+
+    wishlist_items = wishlist.wishlistitem_set.all()
+
+    context = {
+        'wishlist_items': wishlist_items,
+    }
+    return render(request, 'wishlist/wishlist.html', context)
